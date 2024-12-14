b16be84ca45ef15a41bbcb2d378357c5a3a70a7f
Ethan Weisberg <ethaniweisberg@gmail.com>
2024-12-14 14:05:23 -0500

    fixed path

--- api/celery_worker/main.py
+++ api/celery_worker/main.py
@@ -5,7 +5,7 @@ broker_url = os.getenv("CELERY_BROKER_URL")
 backend_url = os.getenv("CELERY_RESULT_BACKEND")
 
 try:
-    CACHE_REQUIREMENTS_PATH = "cache_requirements.txt"
+    CACHE_REQUIREMENTS_PATH = "/app/cache_requirements.txt"
     with open(CACHE_REQUIREMENTS_PATH) as f:
         CACHED_REQUIREMENTS = set(line.strip().lower() for line in f if line.strip())
     celery_instance = Celery(
